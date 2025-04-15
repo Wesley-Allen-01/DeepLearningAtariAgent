31c69fab1f29ecc896e99d332e86ea84fd6fca98
Jasdeep Singh <jasi5951@colorado.edu>
2025-04-15 13:15:34 -0600

    dqn class

--- models/dqn.py
+++ models/dqn.py
@@ -3,6 +3,27 @@ import torch.nn as nn
 
 class DQN(nn.Module):
     def __init__(self, input_shape, num_actions):
-        ...
+        super(DQN, self).__init__()
+        c, h, w = input_shape # should be 4, 84, 84
+
+        self.conv1 = nn.Conv2d(c, 32, kernel_size=8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+
+        def conv2d_size_out(size, kernel_size, stride, padding=0):
+            return (size - kernel_size + 2 * padding) // stride + 1
+        
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w, 8, 4), 4, 2), 3, 1)
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h, 8, 4), 4, 2), 3, 1)
+        linear_input_size = convw * convh * 64
+
+        self.fc1 = nn.Linear(linear_input_size, 512)
+        self.fc2 = nn.Linear(512, num_actions)
+
     def forward(self, x):
-        ...
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        return self.fc2(x)
