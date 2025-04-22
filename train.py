52a4ab9f26a45b760d36aa847d9ba3f464bfeb56
Wesley Allen <weal2937@colorado.edu>
2025-04-21 22:12:42 -0600

    calling train function begins a standard training loop that saves the model every 1000 episodes, implemented interruption handling so if we get impatent at ctrl-C at any point, the env will still close and the train function will return

--- train.py
+++ train.py
@@ -1,23 +1,36 @@
 # can just hold our training loop in the form of a function that we can import in our main file
 from agent.dqn_agent import DQNAgent
+from tqdm import tqdm
+from torch.utils.tensorboard import SummaryWriter
+import os
+import time
 
 
-
-def train(env, agent, episodes):
+def train(env, agent, episodes, verbose=False):
     rewards = []
-    for episode in range(episodes):
-        observation, info = env.reset()
-        episode_over = False
-        total_reward = 0
-        while not episode_over:
-            action = agent.select_action(observation)
-            new_state, reward, terminated, truncated, info, = env.step(action)
-            total_reward += reward
-            episode_over = terminated or truncated
-            agent.memory.push(observation, action, new_state, reward, episode_over)
-            observation = new_state
-            agent.optimize_model()
-        rewards.append(total_reward)
-        print(f"Episode {episode + 1}/{episodes} - Total Reward: {total_reward}")
-    env.close()
-    return rewards
+    
+    try:
+        for episode in tqdm(range(episodes)):
+            if episode+1 % 1000 == 0:
+                agent.save_model(episode)
+            observation, info = env.reset()
+            episode_over = False
+            total_reward = 0
+            while not episode_over:
+                action = agent.select_action(observation)
+                new_state, reward, terminated, truncated, info, = env.step(action)
+                total_reward += reward
+                episode_over = terminated or truncated
+                agent.memory.push(observation, action, new_state, reward, episode_over)
+                observation = new_state
+                agent.optimize_model()
+            rewards.append(total_reward)
+
+            
+            if verbose and episode % 10 == 0:
+                print(f"Episode {episode + 1}/{episodes} - Total Reward: {total_reward}")
+    except KeyboardInterrupt:
+        print("Training interrupted.") 
+    finally:
+        env.close()
+        return rewards
